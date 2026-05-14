901d5f303db9ec605bc62c50676a1d2f916bb4d3
Ashutosh Singh <ashutosh.singh@inferworks.com>
2026-05-14 18:08:38 +0530

    update

---
+++ typeScript/src/32_access_Modifier_Protected.ts
@@ -0,0 +1,17 @@
+
+class Animal{
+    protected energy = 23
+
+    eat (amount:number){
+        this.energy = Math.min(100,this.energy+amount)
+    }
+}
+
+class Dog extends Animal{
+    run(){
+        this.energy -= 10 
+    }
+    status(){
+        return this.energy
+    }
+}
